7c1734bc6d592cd5fd716c338dcf6622899502ce
ANJALI RAI <115608555+TheAnjaliRai@users.noreply.github.com>
2025-03-26 15:25:18 +0530

    Time: 70 ms (11.95%), Space: 34.1 MB (15.52%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        freq = {}
+        for i in range(len(nums)):
+            if nums[i] in freq:
+                freq[nums[i]] += 1
+            else:
+                freq[nums[i]] = 1
+            
+        for key,value in freq.items():
+            if value >=2:
+                return True
+        return False
