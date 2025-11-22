322e21a4c5a01301c50c0e3a4654d9812acd17a4
ANJALI RAI <115608555+TheAnjaliRai@users.noreply.github.com>
2025-11-22 17:17:25 +0530

    Time: 0 ms (100%), Space: 12.5 MB (92.72%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        sum = 0
+        for i in range(len(nums)):
+            sum = sum + nums[i]
+            nums[i] = sum
+
+        return nums
+
+        
