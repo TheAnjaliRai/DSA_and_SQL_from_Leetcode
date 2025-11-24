e2d9799f94b9541669f58ec0791dd2a7b5c3dce5
ANJALI RAI <115608555+TheAnjaliRai@users.noreply.github.com>
2025-11-24 08:24:18 +0530

    Time: 0 ms (100%), Space: 12.5 MB (9.44%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -7,7 +7,7 @@ class Solution(object):
         """
         write = 0
         for read in range(0,len(nums)):
-            if nums[read]!=val:
+            if nums[read]!=val: #condtion when we want to kepp
                 nums[write]=nums[read]
                 write+=1
         return write
